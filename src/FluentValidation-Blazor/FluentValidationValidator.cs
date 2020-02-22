3ae4cb8556342a60083c9e55efc283ce22a5cde1
kackerman <kevin.arthur.ackerman@gmail.com>
2020-02-22 14:23:50 -0600

    Initial commit

---
+++ src/FluentValidation-Blazor/FluentValidationValidator.cs
@@ -0,0 +1,69 @@
+﻿using FluentValidation.Results;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FluentValidation
+{
+    public class FluentValidationValidator : ComponentBase
+    {
+        [CascadingParameter]
+        private EditContext _editContext { get; set; }
+
+        private ValidationMessageStore _validationMessageStore;
+        
+        [Inject]
+        private ValidatorLocator _validatorLocator { get; set; }
+        
+        public override async Task SetParametersAsync(ParameterView parameters)
+        {
+            EditContext previousEditContext = _editContext;
+
+            await base.SetParametersAsync(parameters);
+
+            if (_editContext == null)
+                throw new NullReferenceException($"{nameof(FluentValidationValidator)} must be placed within an {nameof(EditForm)}");
+
+            if (_editContext != previousEditContext)  EditContextChanged();
+        }
+
+        private void EditContextChanged()
+        {
+            _validationMessageStore = new ValidationMessageStore(_editContext);
+            HookUpEditContextEvents();
+        }
+
+        private void HookUpEditContextEvents()
+        {
+            _editContext.OnValidationRequested += (s,a) => Validate();
+            _editContext.OnFieldChanged += (s, a) => Validate();
+        }
+
+        private void Validate()
+        {
+            _validationMessageStore.Clear();
+
+            var validators = _validatorLocator.GetValidators(_editContext.Model.GetType());
+
+            var context = new ValidationContext(_editContext.Model);
+            var failures = validators
+                .SelectMany(v => v.Validate(context).Errors)
+                .Where(f => f != null)
+                .ToList();
+
+            AddValidationResult(_editContext.Model, new ValidationResult(failures));
+        }
+
+        private void AddValidationResult(object model, ValidationResult validationResult)
+        {
+            foreach (ValidationFailure error in validationResult.Errors)
+            {
+                var fieldIdentifier = new FieldIdentifier(model, error.PropertyName);
+                _validationMessageStore.Add(fieldIdentifier, error.ErrorMessage);
+            }
+            _editContext.NotifyValidationStateChanged();
+        }
+    }
+}
---
+++ src/FluentValidation-Blazor/ValidatorLocator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace FluentValidation
+{
+    public class ValidatorLocator
+    {
+        private static readonly ConcurrentDictionary<Type, IEnumerable<IValidator>> _validatorLocatorCache =
+            new ConcurrentDictionary<Type, IEnumerable<IValidator>>();
+
+        private readonly IServiceProvider _serviceProvider;
+
+        public ValidatorLocator(IServiceProvider serviceProvider) =>
+            _serviceProvider = serviceProvider;
+
+        public IEnumerable<IValidator> GetValidators(Type modelType) =>
+            _validatorLocatorCache.GetOrAdd(
+                modelType, 
+                x => 
+                {
+                    var validatorType = typeof(IValidator<>).MakeGenericType(modelType);
+                    var enumerableType = typeof(IEnumerable<>).MakeGenericType(validatorType);
+                    var validators = new List<IValidator>();
+
+                    foreach(var validator in (IEnumerable)_serviceProvider.GetService(enumerableType))
+                        validators.Add((IValidator)validator);
+
+                    return validators.ToArray();
+                }
+            );
+    }
+}
