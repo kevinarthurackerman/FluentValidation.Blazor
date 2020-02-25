3168199d92e50d3ceb36692e8cb51f3ddcadcd3a
kackerman <kevin.arthur.ackerman@gmail.com>
2020-02-24 23:17:20 -0600

    Adds test project

---
+++ src/FluentValidation.Blazor.Example/Validators/UserValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation.Blazor.Example.Models;
+
+namespace FluentValidation.Blazor.Example.Validators
+{
+    public class UserValidator : AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            RuleFor(x => x.FirstName).NotEmpty();
+            RuleFor(x => x.LastName).NotEmpty();
+        }
+    }
+}
