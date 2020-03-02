b21d98e1fce9475985bfe6f98c162ec3a7b3251b
kackerman <kevin.arthur.ackerman@gmail.com>
2020-03-01 18:32:10 -0600

    Makes validation less aggressive when modifying a field
    Before validation messages would update for the whole model for each and every field you update

--- src/FluentValidation-Blazor/FluentValidationValidator.cs
+++ src/FluentValidation-Blazor/FluentValidationValidator.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -23,7 +24,7 @@ namespace FluentValidation
         public FluentValidationValidator()
         {
             _onValidationRequested = (s, a) => Validate();
-            _onFieldChanged = (s, a) => Validate();
+            _onFieldChanged = (s, a) => Validate(a.FieldIdentifier.FieldName);
         }
         
         public override async Task SetParametersAsync(ParameterView parameters)
@@ -52,24 +53,26 @@ namespace FluentValidation
             _editContext.OnFieldChanged += _onFieldChanged;
         }
 
-        private void Validate()
+        private void Validate(string fieldName = null)
         {
             _validationMessageStore.Clear();
 
             var validators = _validatorLocator.GetValidators(_editContext.Model.GetType());
 
             var context = new ValidationContext(_editContext.Model);
-            var failures = validators
+            var validationQuery = validators
                 .SelectMany(v => v.Validate(context).Errors)
-                .Where(f => f != null)
-                .ToList();
+                .Where(f => f != null);
 
-            AddValidationResult(_editContext.Model, new ValidationResult(failures));
+            if (fieldName != null) validationQuery = 
+                    validationQuery .Where(x => x.PropertyName.Equals(fieldName, StringComparison.OrdinalIgnoreCase));
+
+            AddValidationResult(_editContext.Model, validationQuery.ToArray());
         }
 
-        private void AddValidationResult(object model, ValidationResult validationResult)
+        private void AddValidationResult(object model, IEnumerable<ValidationFailure> validationFailures)
         {
-            foreach (ValidationFailure error in validationResult.Errors)
+            foreach (ValidationFailure error in validationFailures)
             {
                 var fieldIdentifier = new FieldIdentifier(model, error.PropertyName);
                 _validationMessageStore.Add(fieldIdentifier, error.ErrorMessage);
