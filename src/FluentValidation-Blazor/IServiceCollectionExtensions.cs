b857335c870d345ed551cec37d5a289f1d52aaf2
kackerman <kevin.arthur.ackerman@gmail.com>
2020-03-07 10:22:30 -0600

    Simplifies service registration

---
+++ src/FluentValidation-Blazor/IServiceCollectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace FluentValidation
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddFluentValidation(this IServiceCollection serviceCollection, params Type[] assemblyTypeMarkers) =>
+            AddFluentValidation(serviceCollection, assemblyTypeMarkers, ServiceLifetime.Transient);
+
+        public static IServiceCollection AddFluentValidation(this IServiceCollection serviceCollection, IEnumerable<Type> assemblyTypeMarkers, ServiceLifetime serviceLifetime) =>
+            AddFluentValidation(serviceCollection, assemblyTypeMarkers.Select(x => x.Assembly), serviceLifetime);
+
+        public static IServiceCollection AddFluentValidation(this IServiceCollection serviceCollection, params Assembly[] assemblies) =>
+            AddFluentValidation(serviceCollection, assemblies, ServiceLifetime.Transient);
+
+        public static IServiceCollection AddFluentValidation(this IServiceCollection serviceCollection, IEnumerable<Assembly> assemblies, ServiceLifetime serviceLifetime)
+        {
+            serviceCollection.AddValidatorsFromAssemblies(assemblies, serviceLifetime);
+            serviceCollection.AddSingleton<ValidatorLocator>();
+            return serviceCollection;
+        }
+    }
+}
